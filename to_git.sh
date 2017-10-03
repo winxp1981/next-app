b52284d50989efdfcd24a0597ac5bef0d9101902
morris <r92094@csie.ntu.edu.tw>
2017-10-04 00:33:28 +0800

    .

--- to_git.sh
+++ to_git.sh
@@ -4,3 +4,8 @@ git add README.md
 git commit -m ""
 git remote add origin https://github.com/winxp1981/next-app.git
 git push -u origin master
+
+git add .
+git reset -- node_modules/
+git commit -m ""
+git push -u origin master
